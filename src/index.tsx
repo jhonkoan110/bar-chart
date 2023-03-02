ddc8d474c836b4aef906dc059b126e2fb57195ac
Кирилл Капитальцев <jhonkoan110@gmail.com>
2023-03-02 08:38:58 +0300

    dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
+import App from './App';
+import { createRoot } from 'react-dom/client';
 import { BarChartProps as SourceBarChartProps, DataType as SourceDataType } from './types/BarChart';
-export { BarChart } from './components/BarChart';
+import './index.css';
 
+const container = document.getElementById('root');
+
+createRoot(container as HTMLDivElement).render(<App />);
+
+export { BarChart } from './components/BarChart';
 export type BarChartProps = SourceBarChartProps;
 export type DataType = SourceDataType;
